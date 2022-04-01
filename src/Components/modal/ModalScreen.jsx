e4bb4282690cd4a05a0af9781679eb838f299ec3
Shubham Jadhav <38340396+shubham168@users.noreply.github.com>
2022-04-01 23:41:03 +0530

    Update ModalScreen.jsx

--- src/Components/modal/ModalScreen.jsx
+++ src/Components/modal/ModalScreen.jsx
@@ -449,7 +449,7 @@ export const ModalScreen = ({ open, handleOpen, handleClose }) => {
                     )}
                   />
                   <GoogleLogin
-                    clientId="512803252250-beprdg6clce6ekr0lv9e044glqn2k5o2.apps.googleusercontent.com"
+                    clientId="512803252250-377j9aloa5naoi0e4690nvkm1o4e1ajv.apps.googleusercontent.com"
                     buttonText="Google"
                     render={(renderProps) => (
                       <Button
@@ -646,7 +646,7 @@ export const ModalScreen = ({ open, handleOpen, handleClose }) => {
                     )}
                   />
                   <GoogleLogin
-                    clientId="512803252250-beprdg6clce6ekr0lv9e044glqn2k5o2.apps.googleusercontent.com"
+                    clientId="512803252250-377j9aloa5naoi0e4690nvkm1o4e1ajv.apps.googleusercontent.com"
                     buttonText="Google"
                     render={(renderProps) => (
                       <Button
