cff985c86f1a9d930023de21c6fe7d666f4808d7
Satya12325 <satya12325@gmail.com>
2023-04-19 00:18:13 +0530

    Updates

--- src/Redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import {appReducer} from "./Allproduct/reducer"
-import {cart_reducer} from "./Cacrt/cart.reducer"
-export const rootReducer = combineReducers({
-   
-    app: appReducer,
-    cart: cart_reducer,
-    
-})
